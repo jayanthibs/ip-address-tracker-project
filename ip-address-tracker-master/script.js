0c0ddd02d120c122196cc91d8f4d2712ce753df5
Jayanthi Balasubramani <jayanthibs@gmail.com>
2026-02-02 14:49:12 -0500

    added code to index.html, style.css

---
+++ ip-address-tracker-master/script.js
@@ -0,0 +1,17 @@
+import {key} from "./secret.js"
+
+
+
+
+
+
+// //Here we create a map in the 'map' div, add tiles of our choice, and then add a marker with some text in a popup:
+// var map = L.map('map').setView([51.505, -0.09], 13);
+
+// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(map);
+
+// L.marker([51.5, -0.09]).addTo(map)
+//     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+//     .openPopup();
