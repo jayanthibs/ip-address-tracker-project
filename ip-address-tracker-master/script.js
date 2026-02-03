676b3879bdbab7d015d4cd199105159cd694ca25
Jayanthi Balasubramani <jayanthibs@gmail.com>
2026-02-02 20:26:16 -0500

    added fetchAPIData functionality to fetch API Datas based on IP Address

--- ip-address-tracker-master/script.js
+++ ip-address-tracker-master/script.js
@@ -1,18 +1,22 @@
+//importing secret api key from secret.js file
 import { key } from "./secret.js";
+//selecting elements and assigning it to variables
 const searchForm = document.getElementById("search-form");
 const ipAddressInput = document.getElementById("ipAddressInput");
 const searchButton = document.getElementById("search-button");
-
+//selecting span element to show error messages
 const inputError = document.getElementById("inputError");
 
 const apiData = document.getElementById("api-data");
 const map = document.getElementById("map");
 
+//adding validateIPAddress function to validate the input filed
 function validateIPAddress() {
-  const regex = "^(?>(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.){3}(?1)$";
+  const regex =
+    /^(?:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)\.){3}(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)$/;
   if (ipAddressInput.validity.valueMissing) {
     ipAddressInput.setCustomValidity("IP Address is required");
-  } else if (ipAddressInput.value !== regex) {
+  } else if (!regex.test(ipAddressInput.value)) {
     ipAddressInput.setCustomValidity("Enter valid IP Address.");
   } else {
     ipAddressInput.setCustomValidity("");
@@ -33,13 +37,35 @@ searchForm.addEventListener("submit", function (event) {
   if (!isIPAddressValid) {
     return;
   }
+//calling the function to fetch API Data
+  fetchAPIData(ipAddressInput.value);
 });
 
 
-
-
-
-
+//function to fetch API Data
+async function fetchAPIData(ipAddress) {
+  try {
+    const response = await fetch(
+      `https://geo.ipify.org/api/v2/country,city?apiKey=${key}&ipAddress=${ipAddress}`,
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log(data);
+    console.log(
+      `ip: ${data.ip}\nLocation: ${data.location.city}, ${data.location.region}\n${data.location.postalCode}\nTimeZone: UTC${data.location.timezone}\nisp: ${data.isp}`);
+    
+  } catch (error) {
+    if (error instanceof NetworkError) {
+      console.log("Network Error", error.message);
+    } else if (error instanceof DataError) {
+      console.log("Data Error", error);
+    } else {
+      console.error("Unknown Error:", error);
+    }
+  }
+}
 
 
 // //Here we create a map in the 'map' div, add tiles of our choice, and then add a marker with some text in a popup:
